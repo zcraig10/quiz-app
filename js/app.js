f518aecd571270d1f1bb1094cc1d3317640b7ea9
Zack Craig <zack.e.craig@gmail.com>
2015-12-31 13:50:29 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+	/*--- Display information modal box ---*/
+  	$(".instructions").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+});
